70991e3c0390400615ecd82c6b3adee2abe525f2
josenyldo <josenildo.simao@gmail.com>
2021-05-23 21:08:03 -0300

    Testando Task.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+void MostrarInfor(void *pvParameters){
+  while(true)
+    Serial.println("OI");
+  
+  
+  }
 
+
+
+void setup() {
+  Serial.begin(9600);
+xTaskCreatePinnedToCore(
+  MostrarInfor,
+  "MostrarInfor",
+  10000,
+  NULL,
+  1,
+  NULL,
+  1
+  );
 }
 
 void loop() {
